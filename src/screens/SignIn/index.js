40dd027aaebeb9bb13d1c20ce531baaf7d3e1a2f
Claudio Lins <69011104+Claudio-Lins@users.noreply.github.com>
2021-06-01 19:54:14 +0100

    .

---
+++ src/screens/SignIn/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Text} from 'react-native'
+import { Container } from './styles';
+
+export default () => {
+    return(
+        <container>
+            <Text>SignIn</Text>
+        </container>
+    )
+}
